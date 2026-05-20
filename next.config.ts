ae22a121ddddb282e7e057fb4e0476073c345656
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2026-05-20 18:05:02 -0300

    feature: valeu por ensinar sobre o ngrok napolas

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ['unknown-sanction-shindig.ngrok-free.dev'],
 };
 
 export default nextConfig;
